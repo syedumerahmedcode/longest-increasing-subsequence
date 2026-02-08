771fd3273d2c499da15bb51b3509788363763f68
umer <syedumerahmedcode@gmail.com>
2026-02-08 06:56:54 +0500

    Feature: Implemneted the solution and tested it against a valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+        
+        
+        
+        Example 1:
+        
+        Input: nums = [10,9,2,5,3,7,101,18]
+        Output: 4
+        Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+        Example 2:
+        
+        Input: nums = [0,1,0,3,2,3]
+        Output: 4
+        Example 3:
+        
+        Input: nums = [7,7,7,7,7,7,7]
+        Output: 1
+         */
+        int[] nums = { 10, 9, 2, 5, 3, 7, 101, 18 };
+        Solution solution = new Solution();
+        int result = solution.lengthOfLIS(nums);
+        System.out.println("The longest increasing subsequence for the input given above is: "+result);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,27 @@
+
+import java.util.Arrays;
+
 public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        /**
+         * solved using dynamic programming
+         * Time:O(n²)
+         */
+        int[] lis = new int[nums.length];
+        Arrays.fill(lis, 1);
+
+        int max = 1;
+        for(int i=1; i<nums.length; i++){
+            for(int j=0; j<i; j++){
+                 if(nums[i] > nums[j]){
+                    lis[i] = Math.max(lis[i], 1+lis[j]);
+                    max = Math.max(max, lis[i]);
+                }
+            }
+        }
+        return max; 
+
+
+    }
 
 }
